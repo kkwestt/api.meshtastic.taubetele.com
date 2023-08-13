9a3c72666f316005894735d257b256eff883ff37
Constantine T <tokontau@gmail.com>
2023-08-13 20:37:02 +0300

    1
    1

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+// const fs = require('fs')
 const mqtt = require('mqtt')
 const servers = require('./servers.js')
+const protobuf = require('protobufjs')
 
 const devices = {}
 
 const callbacks = (server) => ({
   connect: (client) => () => {
     // console.debug(`Trying to connect to ${server.name}`)
-    client.subscribe('msh/2/#', (err) => {
+    client.subscribe('#', (err) => {
       if (err) {
         throw err
       }
@@ -19,11 +21,41 @@ const callbacks = (server) => ({
     let parsed
     try {
       parsed = JSON.parse(buffer.toString())
+      // console.debug('---')
       // console.debug(parsed)
     } catch (any) {
-      console.debug(buffer.toString());
+      // console.debug('---')
+
+      // protobuf.load('./protobufs/meshtastic/mesh.proto', function (err, root) {
+      //   if (err) {
+      //     console.error('Error loading .proto file:', err)
+      //     return
+      //   }
+      //   try {
+      //     const ExampleMessage = root.lookupType('meshtastic.Position')
+      //     const decodedMessage = ExampleMessage.decode(buffer)
+      //     console.log(decodedMessage)
+      //   } catch (any) { }
+      // })
+
+      protobuf.load('./protobufs/meshtastic/mqtt.proto', function (err, root) {
+        if (err) {
+          console.error('Error loading .proto file:', err)
+          return
+        }
+        try {
+          const ExampleMessage = root.lookupType('meshtastic.ServiceEnvelope')
+          const decodedMessage = ExampleMessage.decode(buffer)
+          console.log(decodedMessage)
+        } catch (any) { }
+      })
+
       // console.debug('Cant parse message, skipping')
       // console.debug(buffer.toString())
+      try { // throw new Error()
+      } catch (any) {
+        console.log(any.message ? any.message : any)
+      }
       return false
     }
 
@@ -39,7 +71,7 @@ const callbacks = (server) => ({
 
     if (!devices[parsed.from]) {
       devices[parsed.from] = { [parsed.type]: parsed, server: server.name }
-    } else if (parsed.payload.temperature) {
+    } else if (parsed?.payload?.temperature) {
       devices[parsed.from].telemetry2 = parsed
     } else {
       devices[parsed.from][parsed.type] = parsed
@@ -47,8 +79,14 @@ const callbacks = (server) => ({
     devices[parsed.from].timestamp = parsed.timestamp
 
     if (!devices[parsed.from].mqtt && parsed?.payload?.id == parsed.sender) {
-      console.debug(`Sender ${parsed.sender}`)
+      // console.debug(`Sender ${parsed.sender}`)
       devices[parsed.from].mqtt = 'online'
+    }
+
+    // {"channel":0,"from":-1812372104,"id":383448021,"payload":{"text":"\u0000"},"sender":"!93f96578","timestamp":1688759976,"to":-1,"type":"text"}
+
+    if (parsed.type == 'text') {
+      console.debug(`TEXT: ${parsed.from}: ${parsed.payload.text}`)
     }
   }
 })
